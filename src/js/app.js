ce6131634c2eb2e07a8431384d6a1794e96f0ad1
annaLucian <garielapoves01@gmail.com>
2023-12-03 19:15:59 +0100

    "test"

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,6 @@
-const kick = document.querySelector(".instrument-container__kick");
-const snare = document.querySelector(".instrument-container__snare");
-const floor = document.querySelector(".instrument-container__floor");
-const tomOne = document.querySelector(".instrument-container__toom--one");
-const tomTwo = document.querySelector(".instrument-container__toom--two");
-const tomThree = document.querySelector(".instrument-container__toom--three");
-//aqui estan las constantes de la animacion
-const ride = document.querySelector(".instrument-container__ride");
-const rideBack = document.querySelector(".cymbal-ride-back");
-
-const closeHh = document.querySelector(".instrument-container__closehh");
-const closeHhBack = document.querySelector(".cymbal-closehh-back");
-
-const openHh = document.querySelector(".instrument-container__openhh");
-const openHhBack = document.querySelector(".cymbal-openhh-back");
-
-const crashOne = document.querySelector(".instrument-container__crash--one");
-const crashOneBack = document.querySelector(".cymbal-crash--one-back");
-
-const crashTwo = document.querySelector(".instrument-container__crash--two");
-const crashTwoBack = document.querySelector(".cymbal-crash--two-back");
-
-const pedal = document.querySelector(".instrument-container__pedal");
-//Baquetas
-const rightStick = document.querySelector(".right-drumstick")
-const leftStick = document.querySelector(".left-drumstick")
 
 
-
+export let audioInstances = {};
 const positionTicks = (instrument, stick, space = 0) => {
   const coords = instrument.getBoundingClientRect()
   // obtengo la coordenada del centro del instrumento
@@ -36,10 +10,11 @@ const positionTicks = (instrument, stick, space = 0) => {
   const posicionTop = instrument.offsetTop;
   const posicionLeft = instrument.offsetLeft;
 
-  // posiciono el stick 
+  // posiciono el stick
   stick.style.top = center + posicionTop + "px"
   stick.style.left = center - space + posicionLeft + "px"
   stick.style.zIndex = 9
+  console.log({ instrument, stick })
 
 }
 
@@ -47,6 +22,7 @@ const playInstrumentSound = (instrument) => {
   // Recupero el audio previamente cargado en el onload
   const audioSrc = instrument.children[0].src
   let audio = audioInstances[audioSrc];
+  console.log({ audio, audioSrc, audioInstances })
   audio.currentTime = 0;
   audio.play();
 }
@@ -73,6 +49,7 @@ const animateCymbals = (instrument, imgBack) => {
     imgBack.classList.add("hitCrash")
   }, 150);
 }
+
 const playKickInstrument = (instrument) => {
   playInstrumentSound(instrument)
   animateKickAndToms(instrument)
@@ -92,53 +69,51 @@ const playCymbals = (instrument, imgBack, stick, space = 0) => {
   animateCymbals(instrument, imgBack)
 };
 
-document.addEventListener("keypress", function (event) {
-  //Drums start
-  if (event.code == "KeyP") {
-    playBassDrums(kick);
-  }
 
-  if (event.code == "KeyA") {
-    playBassDrums(tomOne, leftStick, 50);
-  }
-
-  if (event.code == "KeyS") {
-    playBassDrums(tomTwo, leftStick, 50);
-  }
-
-  if (event.code == "KeyD") {
-    playBassDrums(tomThree, leftStick, 50);
-  }
-
-  if (event.code == "KeyJ") {
-    playBassDrums(floor, leftStick, 50);
-  }
-
-  if (event.code == "KeyV") {
-    playBassDrums(snare, leftStick, 50);
-  }
-  //Cymbals start
-  if (event.code == "KeyQ" || event.code == "KeyW") {
-    playCymbals(crashOne, crashOneBack, leftStick, 50);
-  }
-
-  if (event.code == "KeyT" || event.code == "KeyY") {
-    playCymbals(crashTwo, crashTwoBack, leftStick, 50);
-  }
-
-  if (event.code == "KeyI") {
-    playCymbals(ride, rideBack, leftStick, 50);
-  }
-
-  if (event.code == "KeyX") {
-    playCymbals(closeHh, closeHhBack, leftStick, 50);
-  }
-
-  if (event.code == "KeyZ") {
-    playCymbals(openHh, openHhBack, leftStick, 50);
-  }
-});
 document.addEventListener("DOMContentLoaded", () => {
+
+
+  let audioElements = document.querySelectorAll('audio');
+
+  audioElements.forEach((audioElement) => {
+    const ulrAudio = audioElement.src
+    let audio = new Audio(ulrAudio); // te crea un elemento audio
+
+    audio.load();
+    audioInstances[ulrAudio] = audio;
+
+  });
+
+  const kick = document.querySelector(".instrument-container__kick");
+
+  const snare = document.querySelector(".instrument-container__snare");
+  const floor = document.querySelector(".instrument-container__floor");
+  const tomOne = document.querySelector(".instrument-container__toom--one");
+  const tomTwo = document.querySelector(".instrument-container__toom--two");
+  const tomThree = document.querySelector(".instrument-container__toom--three");
+  //aqui estan las constantes de la animacion
+  const ride = document.querySelector(".instrument-container__ride");
+  const rideBack = document.querySelector(".cymbal-ride-back");
+
+  const closeHh = document.querySelector(".instrument-container__closehh");
+  const closeHhBack = document.querySelector(".cymbal-closehh-back");
+
+  const openHh = document.querySelector(".instrument-container__openhh");
+  const openHhBack = document.querySelector(".cymbal-openhh-back");
+
+  const crashOne = document.querySelector(".instrument-container__crash--one");
+  const crashOneBack = document.querySelector(".cymbal-crash--one-back");
+
+  const crashTwo = document.querySelector(".instrument-container__crash--two");
+  const crashTwoBack = document.querySelector(".cymbal-crash--two-back");
+
+  const pedal = document.querySelector(".instrument-container__pedal");
+  //Baquetas
+  const rightStick = document.querySelector(".right-drumstick")
+  const leftStick = document.querySelector(".left-drumstick")
+
+
+
   kick.addEventListener("mousedown", () => playKickInstrument(kick));
   pedal.addEventListener("mousedown", () => playKickInstrument(kick));
   snare.addEventListener("mousedown", () => playBassDrums(snare, rightStick));
@@ -151,22 +126,67 @@ document.addEventListener("DOMContentLoaded", () => {
   ride.addEventListener("mousedown", () => playCymbals(ride, rideBack, rightStick));
   closeHh.addEventListener("mousedown", () => playCymbals(closeHh, closeHhBack, rightStick));
   openHh.addEventListener("mousedown", () => playCymbals(openHh, openHhBack, rightStick));
+  document.addEventListener("keypress", function (event) {
+    //Drums start
+    if (event.code == "KeyP") {
+      playBassDrums(kick);
+    }
+
+    if (event.code == "KeyA") {
+      playBassDrums(tomOne, leftStick, 50);
+    }
+
+    if (event.code == "KeyS") {
+      playBassDrums(tomTwo, leftStick, 50);
+    }
+
+    if (event.code == "KeyD") {
+      playBassDrums(tomThree, leftStick, 50);
+    }
+
+    if (event.code == "KeyJ") {
+      playBassDrums(floor, leftStick, 50);
+    }
+
+    if (event.code == "KeyV") {
+      playBassDrums(snare, leftStick, 50);
+    }
+    //Cymbals start
+    if (event.code == "KeyQ" || event.code == "KeyW") {
+      playCymbals(crashOne, crashOneBack, leftStick, 50);
+    }
+
+    if (event.code == "KeyT" || event.code == "KeyY") {
+      playCymbals(crashTwo, crashTwoBack, leftStick, 50);
+    }
+
+    if (event.code == "KeyI") {
+      playCymbals(ride, rideBack, leftStick, 50);
+    }
+
+    if (event.code == "KeyX") {
+      playCymbals(closeHh, closeHhBack, leftStick, 50);
+    }
+
+    if (event.code == "KeyZ") {
+      playCymbals(openHh, openHhBack, leftStick, 50);
+    }
+  });
 
 })
 // Pre-cargamos los sonidos para que no exista delay al tocarlos
-let audioInstances = {};
 
-window.onload = () => {
-  let audioElements = document.querySelectorAll('audio');
 
-  audioElements.forEach((audioElement) => {
-    const ulrAudio = audioElement.src
-    let audio = new Audio(ulrAudio); // te crea un elemento audio
+// window.onload = () => {
+//   let audioElements = document.querySelectorAll('audio');
 
-    audio.load();
-    audioInstances[ulrAudio] = audio;
+//   audioElements.forEach((audioElement) => {
+//     const ulrAudio = audioElement.src
+//     let audio = new Audio(ulrAudio); // te crea un elemento audio
 
-  });
-};
+//     audio.load();
+//     audioInstances[ulrAudio] = audio;
 
-console.log(audioInstances)
+//   });
+// };
+export { playBassDrums, playCymbals, playKickInstrument, playInstrumentSound }
