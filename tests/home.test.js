e06bd85292b188d48198f822ef448a560428b67f
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2023-12-04 12:09:35 +0100

    enlaces a páginas instumento, about y en construccion añadido al testing

--- tests/home.test.js
+++ tests/home.test.js
@@ -9,6 +9,7 @@ describe("page home test", () => {
     let dom
     let window
     let button
+    let buttonInstrument
     beforeAll(async () => {
         dom = await JSDOM.fromFile("./index.html", {
             resources: "usable", 
@@ -17,7 +18,8 @@ describe("page home test", () => {
         window = dom.window
         document = dom.window.document
         button = document.querySelector(".container-home__hero-text-block__primary-button");
-        console.log(button.href)
+        buttonInstrument = document.querySelector(".green .container-home__gallery__row__card__secondary-button");
+        console.log(buttonInstrument)
     })
 
     it("should render css", async () => {
@@ -29,5 +31,29 @@ describe("page home test", () => {
     it("should link to about us page", () => {
         expect(button.href).toMatch(/about-us.html$/);
     }) 
+
+    it("should link to instrument page", () => {
+        expect(buttonInstrument.href).toMatch(/instrument.html$/);   
+    }) 
+
+    it("should link to piano in construction page", () => {
+        let buttonPiano = document.querySelector(".violet .container-home__gallery__row__card__secondary-button")
+        expect(buttonPiano.href).toMatch(/in-construccion-piano.html$/);  
+
+    }) 
+
+    it("should link to accordeon in construction page", () => {
+        let buttonAccordeon = document.querySelector(".brown .container-home__gallery__row__card__secondary-button")
+        expect(buttonAccordeon.href).toMatch(/in-construction-accordeon.html$/);  
+
+    }) 
+
+    it("should link to guitar in construction page", () => {
+        let buttonGuitar = document.querySelector(".red .container-home__gallery__row__card__secondary-button")
+        expect(buttonGuitar.href).toMatch(/in-construction-guitar.html$/);  
+
+    }) 
+
 })
+
 
