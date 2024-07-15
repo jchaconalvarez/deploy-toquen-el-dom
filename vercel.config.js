61e08bb9251c1d49904aac5f0607183030cb3159
jlamenza <jlamenza@gmail.com>
2024-07-15 12:42:56 +0200

    testing

--- vercel.config.js
+++ vercel.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
+
 export default defineConfig({
   build: {
     outDir: 'dist'
+  },
+  server: {
+    historyApiFallback: true // This ensures proper SPA routing in development
   }
 });
